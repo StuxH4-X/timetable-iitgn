a0f6e4292f09db12162f04ae438b6e3481702557
StuxH4-X <soleviewiitgn@hotmail.com>
2025-04-06 13:02:02 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ const exportButton = document.getElementById("exportButton");
 const searchBar = document.getElementById("searchBar");
 
 const headers = [
-    "Course Number", "Course Name", "L", "T", "P", "C",
+    "Course Code", "Course Name", "L", "T", "P", "C",
     "Name of the Instructors and Tutors", "Lecture", "Tutorial", "Lab", "HSS/BS elective"
 ];
 
